d9588d5c9ce7487ad9e9e31aeb541f11a7bff9c4
100mgml@gmail.com <100mgml@gmail.com>
2023-02-05 17:53:43 +0900

    feat: Implement shared raw editor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import CodeMirror from '@uiw/react-codemirror';
-import { pythonLanguage } from '@codemirror/lang-python';
-
-const pyLang = `# Press Ctrl-Space in here...`;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Editor from "./Editor";
+import "./App.css";
 
 export default function App() {
-  return <CodeMirror value={pyLang} height="200px" extensions={[pythonLanguage]} />;
+  return (
+    <div className="App">
+      <h1 className="m-10">모각코 메타버스 만세</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Editor />} />
+          <Route path="/:id" element={<Editor />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
---
+++ src/Editor.js
@@ -0,0 +1,133 @@
+import * as Y from "yjs";
+import { CodemirrorBinding } from "y-codemirror";
+import { WebrtcProvider } from "./y-webrtc";
+import CodeMirror from "codemirror";
+import { useEffect, useState } from "react";
+import short from "short-uuid";
+import { useParams, useNavigate } from "react-router-dom";
+import "codemirror/mode/javascript/javascript.js";
+import "codemirror/addon/mode/simple.js";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material-darker.css";
+import mode from "./mode-glicol";
+import { randomHex } from "randomize-hex";
+
+const Editor = () => {
+    CodeMirror.defineSimpleMode("simplemode", mode);
+    const [name, setName] = useState("이름");
+    const [color, setColor] = useState("#008833");
+    let navigate = useNavigate();
+    let { id } = useParams();
+    console.log(id);
+    const connect = (room) => {
+        try {
+            const ydoc = new Y.Doc();
+            const provider = new WebrtcProvider(room, ydoc);
+            const yText = ydoc.getText("codemirror");
+            const yUndoManager = new Y.UndoManager(yText);
+
+            const e = new CodeMirror(document.getElementById("editor"), {
+                mode: "simplemode",
+                lineNumbers: true,
+                theme: "material-darker"
+            });
+            window.binding = new CodemirrorBinding(yText, e, provider.awareness, {
+                yUndoManager
+            });
+            console.log("connect to", room);
+            window.binding.awareness.setLocalStateField("user", {
+                color: color,
+                name: name
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const copy = () => {
+        let copyText = document.getElementById("myInput");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        alert("Link copied!");
+    };
+
+    const newRoom = () => {
+        let uuid = short.generate();
+        // connect(uuid);
+        navigate(uuid);
+    };
+
+    useEffect(() => {
+        if (id) {
+            connect(id);
+        }
+        console.log("effect");
+    }, [id]);
+    return (
+        <div>
+            {id ? (
+                <>
+                    <p style={{ display: "inline" }}>
+                        다음 링크를 복사해서 다른 사람 초대하세요:
+
+
+                    </p>
+                    <input
+                        readOnly
+                        type="text"
+                        value={`https://0t7bn.csb.app/${id}`}
+                        size="50"
+                        id="myInput"
+                    />
+                    <button onClick={copy}>copy</button> <br />
+                    <br />
+                    <p style={{ display: "inline" }}>이름 바꿔보세여: </p>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                            window.binding.awareness.setLocalStateField("user", {
+                                color: "#008833",
+                                name: e.target.value
+                            });
+                        }}
+                        size="15"
+                        id="name"
+                    />
+                    <button
+                        style={{ backgroundColor: color }}
+                        onClick={() => {
+                            let c = randomHex();
+                            setColor(c);
+                            window.binding.awareness.setLocalStateField("user", {
+                                color: c,
+                                name: name
+                            });
+                        }}
+                    >
+                        Change a color
+                    </button>
+                </>
+            ) : (
+                <div className="m-10">
+                    <ol>
+                        <li>"IDE 켜기"를 누르세요</li>
+                        <li>생성된 url을 복사하세요</li>
+                        <li>새 창을 키거나 다른 사람을 초대해서 test 해보세요</li>
+                    </ol>
+                    <button
+                        className="hover:bg-indigo-200 bg-indigo-300 text-yellow-800 shadow rounded py-1 px-5"
+                        onClick={newRoom}
+                    >
+                        IDE 켜기
+                    </button>
+                </div>
+            )}
+            <div id="editor"></div>
+        </div>
+    );
+};
+
+export default Editor;
