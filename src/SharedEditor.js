453a1e4b04dfc0248b32a7c3318dbaafef78f9fb
100mgml@gmail.com <100mgml@gmail.com>
2023-02-08 01:02:02 +0900

    fix : Fix double socket generating on client-side

--- src/SharedEditor.js
+++ src/SharedEditor.js
@@ -2,18 +2,15 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useStore } from './store.js';
 
 /* codemirror */
-// import { CodeMirror, useCodeMirror } from '@uiw/react-codemirror';
 import { python } from '@codemirror/lang-python';
-import { basicSetup, keymap, defaultKeymap } from 'codemirror';
-import { EditorState } from '@codemirror/state';
-import { EditorView } from '@codemirror/view';
+import { basicSetup } from 'codemirror';
+import { EditorState, Compartment } from '@codemirror/state';
+import { EditorView, ViewUpdate, ViewPlugin } from '@codemirror/view';
 
 /* socket */
 import io from 'socket.io-client';
 
 function SharedEditor() {
-  /* pylang 플레이스홀더 */
-  const pyLang = `# Press Ctrl-Space in here...\n\n\n\n`;
   const editor = useRef(null);
 
   /* local state 저장 */
@@ -23,35 +20,22 @@ function SharedEditor() {
     roomId,
   })); // useStore에 저장한 것 디스트럭쳐링
 
-  /* useEffect 밖에다가 써봄 */
-  const [state, setState] = useState(
-    EditorState.create({
-      doc: pyLang,
-      extensions: [basicSetup, python()],
-    })
-  );
-
   useEffect(() => {
     /* 에디터 인스턴스 생성 */
-    const log = (event) => console.log(event);
-    // editor.current.addEventListener("input", log);
+    const log = (event) => console.log(event.data);
+    // editor.current.addEventListener('input', log);
 
-    // const state = EditorState.create({
-    //   doc: pyLang,
-    //   extensions: [basicSetup, python()],
-    // });
-
-    const view = new EditorView({
-      state,
-      parent: editor.current,
-      update: (update) => {
-        update.on(update.transactions, (tr) => {
-          state.dispatch(tr);
-          const code = state.toString();
-          socket.emit('CODE_CHANGED', code);
-        });
-      },
+    const state = EditorState.create({
+      doc: `# Press Ctrl-Space in here... 포항항\n\n\n\n`,
+      extensions: [basicSetup, python()],
     });
+
+    /* view의 update 메소드를 통해 내용 바꿈 test */
+    // let transaction = state.update({
+    //   changes: { from: 0, insert: 'hello' },
+    // });
+    // console.log(transaction.state.doc.toString()); // "hello editor"
+    // view.dispatch(transaction);
 
     /* hmmm 위젯 */
     // const widget = document.createElement('span');
@@ -64,14 +48,20 @@ function SharedEditor() {
     // widget.onclick = () => bookMark.clear()
     // console.log(editor.getAllMarks())
 
+    // 서버로부터 "코드변경" 알림 받음
+
     /* 소켓 연결!!! */
     const socket = io('http://localhost:3001/', {
       transports: ['websocket'],
     });
 
-    // 서버로부터 "코드변경" 알림 받음
     socket.on('CODE_CHANGED', (code) => {
       console.log(code);
+      if (code !== view.state.doc.toString()) {
+        view.dispatch({
+          changes: { from: 0, to: view.state.doc.length, insert: code },
+        });
+      }
     });
 
     socket.on('connect_error', (err) => {
@@ -80,7 +70,7 @@ function SharedEditor() {
 
     /* 클라이언트가 서버소켓에 연결되면 "방 연결" 이라고 알림  */
     socket.on('connect', () => {
-      console.log('접속 됨');
+      // console.log('접속 됨');
       socket.emit('CONNECTED_TO_ROOM', { roomId, username });
     });
 
@@ -90,27 +80,56 @@ function SharedEditor() {
 
     /* 서버에서 "방 연결" 알림 받으면 */
     socket.on('ROOM:CONNECTION', (users) => {
-      console.log('방 연결 됨');
+      // console.log('방 연결 됨');
       setUsers(users); // 유저 정보 변경
       console.log(`유저정보리스트 update : ${users}`);
     });
 
-    /* 코드미러가 변화를 알려줌(입력, 삭제, 등) */
-    // editor.on('change', (instance, changes) => {
+    /* 에디터가 변화를 알려줌(입력, 삭제, 등) */
+    // editor.on('change', (state, changes) => {
     //   const { origin } = changes;
     //   // if (origin === '+input' || origin === '+delete' || origin === 'cut') {
-    //   if (origin !== 'setValue') {
-    //     // setValue라는 변화가 아니면
-    //     socket.emit('CODE_CHANGED', instance.getValue()); // 코드변경 이벤트 알림
+    //   if (origin !== 'setValue') {  // setValue라는 변화가 아니면
+    //     socket.emit('CODE_CHANGED', state.doc.toString()); // 코드변경 이벤트 알림
     //   }
     // });
 
-    // editor.on('cursorActivity', (instance) => {
-    //   // console.log(instance.cursorCoords())
+    /* cursor activity 이벤트 있던 곳 */
+
+    // editor.current.addEventListener('input', (e) => {
+    //   // console.log(e);
+    //   if ()
+    //   socket.emit('CODE_CHANGED', state.doc.toString());
     // });
+
+    const MyViewPlugin = {
+      // The update method will be called whenever there is a change to the view
+      update(view, prevState) {
+        // Only emit the socket event if the value of the editor changed
+        if (prevState.state.doc.toString() !== view.state.doc.toString()) {
+          // Emit the socket event with the current value of the editor
+          socket.emit('CODE_CHANGED', view.state.doc.toString());
+        }
+      },
+    };
+
+    const view = new EditorView({
+      state,
+      plugins: [MyViewPlugin],
+      parent: editor.current,
+      // update: (update) => {
+      //   update.on(update.transactions, (tr) => {
+      //     state.dispatch(tr);
+      //     const code = state.toString();
+      //     console.log('코드바뀜');
+      //     socket.emit('CODE_CHANGED', code);
+      //   });
+      // },
+    });
 
     return () => {
       socket.emit('DISSCONNECT_FROM_ROOM', { roomId, username });
+      socket.close(); // 2명 되는거 방지
       view.destroy(); // view가 여러개 생기지 못하게 방지
       // view.current.removeEventListener("input", log);
     };
@@ -126,7 +145,7 @@ function SharedEditor() {
         {/* <div>Your username is: 네이름</div> */}
         {/* <div>The room ID is: 네 아이디</div> */}
         {/* <div>How many pople are connected: 몇명</div> */}
-        <div ref={editor} state={state} />
+        <div ref={editor} />
       </div>
     </>
   );
